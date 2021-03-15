00c224c72cffb51ae8909ba45069c47105106303
Abhi Jain <devabhixda@gmail.com>
2021-03-15 10:52:03 +0530

    Add temperature sensing
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

---
+++ Temperature/README.md
@@ -0,0 +1,4 @@
+# Temperature 
+### Temperature Sensing with Arduino
+
+<p align="center"> <img src="https://github.com/devabhixda/IoT_Lab/blob/master/Temperature/Temperature.png">  </p>
---
+++ Temperature/Temperature.ino
@@ -0,0 +1,17 @@
+int sensorInput;
+double temp;
+ 
+void setup() {
+  Serial.begin(9600);
+ 
+}
+void loop() {
+  sensorInput = analogRead(A0);  
+  temp = (double)sensorInput / 1024; 
+  temp = temp * 5;           
+  temp = temp - 0.5;            
+  temp = temp * 100;          
+ 
+  Serial.print("Current Temperature: ");
+  Serial.println(temp);
+}
